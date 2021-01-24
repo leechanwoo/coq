f9e9ad12b5b6115b35ed4859a63750ac291cb420
leechanwoo <cjswosa2@gmail.com>
2021-01-24 10:19:52 +0900

    chapter 1 finished

--- excercise_1.v
+++ excercise_1.v
@@ -57,14 +57,54 @@ Inductive bin : Type :=
 | B0 (n : bin)
 | B1 (n : bin).
 
+Fixpoint incr (m:bin) : bin :=
+  match m with
+  | Z => B1 Z 
+  | B0 m' => B1 m'
+  | B1 m' => B0 (incr m')
+  end.
   
-                          
-            
+                  
+
+Fixpoint bin_to_nat (m:bin) : nat :=
+  match m with
+  | Z => O
+  | B1 Z => S O
+  | B1 m' => S O + bin_to_nat m' + bin_to_nat m'
+  | B0 m' => bin_to_nat m' + bin_to_nat m'
+  end.
+
+Example bin_to_nat1 : bin_to_nat(B0 (B0 (B1 Z))) = S (S (S (S O))).
+Proof. simpl. reflexivity. Qed.
+
+Example bin_to_nat2 : bin_to_nat(B1 (B1 (B1 Z))) = 7.
+Proof. simpl. reflexivity. Qed.
+                       
+Example bin_to_nat3 : bin_to_nat(B0 (B0 (B0 (B1 Z)))) = 8.
+Proof. simpl. reflexivity. Qed.
+
+Example test_bin_incr1 : (incr (B1 Z)) = B0 (B1 Z).
+Proof. simpl. reflexivity. Qed.
 
 
+Example test_bin_incr2 : (incr (B0 (B1 Z))) = B1 (B1 Z).
+Proof. simpl. reflexivity. Qed.
+
+Example test_bin_incr3 : (incr (B1 (B1 Z))) = B0 (B0 (B1 Z)).
+Proof. simpl. reflexivity. Qed.
 
 
+Example test_bin_incr4 : bin_to_nat (B0 (B1 Z)) = 2.
+Proof. simpl. reflexivity. Qed.
 
 
-  
-  
+Example test_bin_incr5 :
+  bin_to_nat (incr (B1 Z)) = 1 + bin_to_nat (B1 Z).
+Proof. simpl. reflexivity. Qed.
+
+
+Example test_bin_incr6:
+  bin_to_nat (incr (incr (B1 Z))) = 2 + bin_to_nat (B1 Z).
+Proof. simpl. reflexivity. Qed.
+
+
